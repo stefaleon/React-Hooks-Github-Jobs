dd8d5c94d0f97f8d5f3089be1809ad3c4fce08b7
stefaleon <stefaleon@gmail.com>
2020-09-23 21:15:30 +0300

    render job card components

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import useFetchJobs from './useFetchJobs';
 
 import { Container } from 'react-bootstrap';
 import './App.css';
+import Job from './Job';
 
 function App() {
   const { jobs, loading, error } = useFetchJobs();
   return (
-    <Container>
+    <Container className='my-4'>
       {loading && <h1>Loading...</h1>}
       {error && <h1>An error occured!</h1>}
       {jobs.map((job) => (
-        <div key={job.id}>{job.title}</div>
+        <Job key={job.id} job={job} />
       ))}
     </Container>
   );
