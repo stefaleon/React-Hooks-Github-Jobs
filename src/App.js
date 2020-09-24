fb1ff71cded7fa4ae696b9aa5bb976873267d58b
stefaleon <stefaleon@gmail.com>
2020-09-24 08:24:03 +0300

    pagination

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useFetchJobs from './useFetchJobs';
 
 import { Container } from 'react-bootstrap';
 import './App.css';
 import Job from './Job';
+import JobsPagination from './JobsPagination';
 
 function App() {
-  const { jobs, loading, error } = useFetchJobs();
+  const [page, setPage] = useState(1);
+  const { jobs, loading, error, hasNextPage } = useFetchJobs(null, page);
   return (
     <Container className='my-4'>
+      <h1 className='mb-4'>React Jobs</h1>
+      <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
       {loading && <h1>Loading...</h1>}
       {error && <h1>An error occured!</h1>}
       {jobs.map((job) => (
--- src/useFetchJobs.js
+++ src/useFetchJobs.js
@@ -5,9 +5,9 @@ const actions = {
   MAKE_REQUEST: 'make-requestto-github-jobs-api',
   GET_DATA: 'get-data-from-github-jobs-api',
   ERROR: 'error',
+  UPDATE_HAS_NEXT_PAGE: 'update-has-next-page',
 };
-
-const { MAKE_REQUEST, GET_DATA, ERROR } = actions;
+const { MAKE_REQUEST, GET_DATA, ERROR, UPDATE_HAS_NEXT_PAGE } = actions;
 
 const BASE_URL =
   'https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json';
@@ -34,6 +34,8 @@ const reducer = (state, action) => {
         error: action.payload.error,
         loading: false,
       };
+    case UPDATE_HAS_NEXT_PAGE:
+      return { ...state, hasNextPage: action.payload.hasNextPage };
     default:
       return state;
   }
@@ -42,21 +44,32 @@ const reducer = (state, action) => {
 const getData = async (dispatch, params, page) => {
   console.log('getData called');
   // handle throttling
-  const cancelToken = axios.CancelToken.source();
+  const cancelToken1 = axios.CancelToken.source();
+  const cancelToken2 = axios.CancelToken.source();
   try {
     dispatch({ type: MAKE_REQUEST });
     const res = await axios.get(BASE_URL, {
-      cancelToken: cancelToken.token,
+      cancelToken: cancelToken1.token,
       params: { ...params, page, markdown: true },
     });
     dispatch({ type: GET_DATA, payload: { jobs: res.data } });
     console.log('got data', res);
+
+    const res2 = await axios.get(BASE_URL, {
+      cancelToken: cancelToken2.token,
+      params: { ...params, page: page + 1, markdown: true },
+    });
+    dispatch({
+      type: UPDATE_HAS_NEXT_PAGE,
+      payload: { hasNextPage: res2.data.length !== 0 },
+    });
   } catch (err) {
     if (axios.isCancel(err)) return;
     dispatch({ type: ERROR, payload: { error: err } });
   }
   return () => {
-    cancelToken.cancel();
+    cancelToken1.cancel();
+    cancelToken2.cancel();
   };
 };
 
